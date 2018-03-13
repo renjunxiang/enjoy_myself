a787c001f3d311fca25038058ad27a3b20a95ae9
renjunxiang <renjunxiang1215@sina.com>
2018-03-13 18:12:57 +0800

    。

---
+++ chatbot/code.py
@@ -0,0 +1,84 @@
+from keras.preprocessing.text import Tokenizer, text_to_word_sequence
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+import numpy as np
+import pandas as pd
+from net.seq2seq import seq2seq
+import jieba
+import os
+from random import sample
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"  # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+
+def reverse_num(x='123456'):
+    y = [x[len(x) - 1 - i] for i in range(len(x))]
+    return ''.join(y)
+
+ask_transform = [' '.join(list(np.random.randint(1, 10, np.random.randint(1, 31)).astype(str))) for i in range(1000)]
+answer_transform = [reverse_num(i) for i in ask_transform]
+
+
+# def texts_transform(texts=None):
+#     texts_new = [' '.join(jieba.lcut(i)) for i in texts]
+#     return texts_new
+#
+#
+# ask_transform = texts_transform(texts=ask)
+# answer_transform = texts_transform(texts=answer)
+
+tokenizer_ask = Tokenizer()
+tokenizer_ask.fit_on_texts(texts=ask_transform)
+ask_seq = tokenizer_ask.texts_to_sequences(texts=ask_transform)
+ask_new = pad_sequences(ask_seq, maxlen=30, padding='post', value=0, dtype='int')
+
+output_len = 30
+
+tokenizer_answer = Tokenizer(num_words=50)
+tokenizer_answer.fit_on_texts(texts=answer_transform)
+answer_seq = tokenizer_answer.texts_to_sequences(texts=answer_transform)
+answer_new = pad_sequences(answer_seq, maxlen=output_len, padding='post', value=0, dtype='int')
+answer_categorical = to_categorical(answer_new)
+
+model_seq2seq = seq2seq(input_dic_len=len(tokenizer_ask.word_index),
+                        input_len=30,
+                        vector_len=50,
+                        hidden_dim=20,
+                        output_dim=answer_categorical.shape[2],
+                        output_len=output_len)
+
+model_seq2seq.fit(x=ask_new, y=answer_categorical, batch_size=50, epochs=5)
+
+answer_key = list(tokenizer_answer.word_index.keys())
+answer_values = list(tokenizer_answer.word_index.values())
+
+
+def robot(text=None):
+    text = [jieba.lcut(text)]
+    text_seq = tokenizer_ask.texts_to_sequences(texts=text)
+    text_new = pad_sequences(text_seq, maxlen=30, padding='post', value=0, dtype='float32')
+    result = model_seq2seq.predict(text_new)[0]
+    result = [np.argmax(i) for i in result]
+    # result=np.random.randint(1,500,np.random.randint(10,50,1)[0])
+    result = ''.join([answer_key[answer_values.index(i)] for i in result if i in answer_values])
+    return result
+
+def num_robot(text=None):
+    text_seq = tokenizer_ask.texts_to_sequences(texts=text)
+    text_new = pad_sequences(text_seq, maxlen=30, padding='post', value=0, dtype='float32')
+    result = model_seq2seq.predict(text_new)[0]
+    result = [np.argmax(i) for i in result]
+    # result=np.random.randint(1,500,np.random.randint(10,50,1)[0])
+    result = ''.join([answer_key[answer_values.index(i)] for i in result if i in answer_values])
+    return result
+
+for i in ask_transform[0:10]:
+    print('ask:', i)
+    print('answer:', num_robot(text=i))
+
+111
+
+model_seq2seq.predict(np.array([np.random.randint(1, 50, 10)]))
+
+answer_key[answer_values.index(5)]
---
+++ chatbot/net/seq2seq_simple.py
@@ -0,0 +1,50 @@
+from keras.models import Sequential
+from keras.layers.recurrent import GRU
+from keras.layers import Bidirectional
+from keras.layers.wrappers import TimeDistributed
+from keras.layers.core import Dense, RepeatVector, Masking
+from keras.layers.embeddings import Embedding
+from keras.optimizers import SGD, Adagrad, Adam
+
+
+def seq2seq_simple(input_dic_len=100,
+                   input_len=50,
+                   vector_len=200,
+                   output_dic_len=100,
+                   output_len=10):
+    '''
+    
+    :param input_dic_len: 输入的字典长度
+    :param input_len: 输入的文本长度
+    :param vector_len: 词向量维度
+    :param output_dic_len: 输出的字典长度
+    :param output_len: 输出的文本长度
+    :return: 
+    '''
+
+    # input_dic_len=100
+    # input_len=50
+    # vector_len=200
+    # output_len=50
+    # output_dic_len=100
+
+    model = Sequential()
+    # 创建词向量
+    model.add(Embedding(input_dim=input_dic_len + 1,
+                        input_length=input_len,
+                        output_dim=vector_len,
+                        mask_zero=0))
+    # encoding过程
+    model.add(Bidirectional(GRU(units=32,
+                                activation='tanh',
+                                recurrent_activation='hard_sigmoid',
+                                return_sequences=False)))
+    model.add(Dense(units=50,
+                    activation='relu'))
+    # decoding过程，按照输出长度复制encoding的结果
+    model.add(RepeatVector(output_len))
+    model.add(GRU(units=50, return_sequences=True, activation="relu"))
+    model.add(TimeDistributed(Dense(units=output_dic_len, activation="relu")))
+    optimizer = Adagrad(lr=0.01)
+    model.compile(optimizer=optimizer, loss='mse', metrics=['accuracy'])
+    return model
